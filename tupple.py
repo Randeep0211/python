9f038bbb525b122c77b29cf1b380ad6e3b311601
Randeep singh <jazzysingh227@gmail.com>
2025-09-17 18:08:33 +0530

    tupple

---
+++ tupple.py
@@ -0,0 +1,6 @@
+#### A tuple is an ordered, immutable collection of elements. Think of it like a list, 
+# but once created, it cannot be changed—no adding, removing, or modifying elements.
+
+ec2_instance = ("instance_1","instance_2",True,1)
+# print(len(ec2_instance))
+# ec2_instance.pop() 
